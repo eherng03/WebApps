1b2bc0dc75bcd388553655e8ba09196bbc20aa35
EvaHergar <eherng03@estudiantes.unileon.es>
2017-03-31 21:11:11 +0200

    Add files via upload

--- Task1/valida.js
+++ Task1/valida.js
@@ -9,5 +9,28 @@ $(document).ready(function(){
 			$(checkName).prop('checked', false);
 		}
 	});
-	$()
+
+	$('td').find('input').change(function() {
+		//mirar a ver si hay alguno seleccionado
+		if($(this).find(':checked').length == 0){
+			$("textarea").hide();
+		}else{
+			$('textarea').show();
+//https://api.jquery.com/change/
+
+		}
+	});
+
+	$('label').click(function() {
+		var sibling = $(this).prev();
+		if($(sibling).prop('checked')){
+			$(sibling).prop('checked', false);
+			$('td').find('input').change();
+		}else{
+			$(sibling).prop('checked', true);
+			$('td').find('input').change();
+		}
+	});
+
+
 });
